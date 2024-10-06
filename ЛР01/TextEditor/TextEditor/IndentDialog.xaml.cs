c3ba301eaf11113a239a3f19af72d1b8fb5919a1
Illia Kotvitskyi <iiiako197@gmail.com>
2024-10-06 14:21:21 +0300

    init commit

---
+++ ЛР01/TextEditor/TextEditor/IndentDialog.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace TextEditor
+{
+    public partial class IndentDialog : Window
+    {
+        public int Indent { get; private set; }
+
+        public IndentDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void OK_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(IndentTextBox.Text, out int indent))
+            {
+                Indent = indent;
+                DialogResult = true;
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid number.", "Invalid Input", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+    }
+}
---
+++ ЛР01/TextEditor/TextEditor/NewTab.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows;
+
+namespace TextEditor
+{
+    public partial class NewTab : Window
+    {
+        public NewTab()
+        {
+            InitializeComponent();
+        }
+
+        public string? FileName { get; set; }
+
+        private void CreateButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (FileNameTextBox.Text != null && !string.IsNullOrWhiteSpace(FileNameTextBox.Text))
+                    FileName = FileNameTextBox.Text;
+                else throw new NullReferenceException();
+
+                DialogResult = true;
+                Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+    }
+}
